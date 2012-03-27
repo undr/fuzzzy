fc23eda0f9f74afb28d175a7342805c7c7be8a8e
undr <undr@yandex.ru>
2012-03-27 20:31:41 +0700

    Исправил баг в методе ngram когда исходный запрос изменяется

--- lib/fuzzzy/methods/ngram/base.rb
+++ lib/fuzzzy/methods/ngram/base.rb
@@ -2,7 +2,7 @@ module Fuzzzy
   module Ngram
     class Base < MethodBase
       def ngrams string=nil
-        string ||= query_index_string
+        string ||= query_index_string.dup
         string.downcase!
         context[string] ||= (0..string.length-3).to_a.collect{|idx| string[idx,3] }
       end
