16ed38be72b2e273d6dcc09385bce0a573111ba2
undr <undr@yandex.ru>
2012-04-05 00:04:48 +0700

    Fix bug: soundex for nil or empty string has to return empty string

--- lib/fuzzzy/methods/soundex/base.rb
+++ lib/fuzzzy/methods/soundex/base.rb
@@ -2,7 +2,7 @@ module Fuzzzy
   module Soundex
     class Base < MethodBase
       def soundex string=nil
-        context[:soundex] ||= Text::Soundex.soundex(string || query_index_string)
+        context[:soundex] ||= Text::Soundex.soundex(string || query_index_string).to_s
       end
 
       def index_type
