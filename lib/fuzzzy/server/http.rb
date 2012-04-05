45128206d41889eb618d5372e8983bb9d94be6c5
undr <undr@yandex.ru>
2012-04-05 21:38:30 +0700

    HTTP server can index, search keywords and show information

--- lib/fuzzzy/server/http.rb
+++ lib/fuzzzy/server/http.rb
@@ -1,17 +1,49 @@
+require 'pp'
 module Fuzzzy
   module Server
     class HTTP < Grape::API
+      class ParamsError < StandardError
+      end
+
+      format :json
       default_format :json
-      version 'v1'
+      error_format :json
+      version 'v1', :using => :path
+
+      rescue_from :all do |e|
+        rack_response({:error => e.class.name, :message => e.message}.to_json)
+      end
 
       helpers do
         include Index
-
-        def default_context
-          {
-            :index_name => params[:index_name],
-            :query => params[:query]
-          }
+        
+        def search
+          context = search_context
+          check_context!(:query, context)
+          _searcher(context[:index_method]).search(context)
+        end
+        
+        def check_context! *keys
+          context = keys.pop
+          ([:index_name, :index_method] + keys).each do |key|
+            raise ParamsError.new("Parameter :#{key} not found") if context[key].nil?
+          end
+        end
+        
+        def assign_context! *keys
+          context = {}
+          ([:index_name, :index_method] + keys).each do |key|
+            context[key] = params[key] if params[key]
+          end
+          context
+        end
+        
+        def index_context *keys
+          assign_context!(:id, :dictionary_string)
+        end
+        
+        def search_context
+          assign_context!(:query, :distance, :sort_by)
         end
       end
 
@@ -22,15 +54,12 @@ module Fuzzzy
 
         get do
           info = {
-            'ruby' => RUBY_VERSION,
-            'environment' => Fuzzzy.env,
-            'redis' => Fuzzzy.redis.client.id,
-            'root_dir' => Fuzzzy.root
+            :ruby => RUBY_VERSION,
+            :environment => Fuzzzy.env,
+            :redis => Fuzzzy.redis.client.id,
+            :root_dir => Fuzzzy.root.to_s
           }
-          if params[:show_config]
-            info['config'] = ZmqJobs.config
-            info['stopwords'] = ZmqJobs.stopwords
-          end
+          info[:stopwords] = Fuzzzy.stopwords if params[:show_stopwords]
           info
         end
 
@@ -41,17 +70,27 @@ module Fuzzzy
           }
         end
       end
-
-      namespace :search do
-        # /search/city:name/ngram?query=Bankok&distance=2&sort_by=distance
-        # /search/user:fullname/soundex?query=Maria%20Huana
-        get '/:index_name/:index_type/' do
-          context = default_context
-          [:distance, :sort_by].each do |key|
-            context[key] = params[key] if params[key]
-          end
-          
-          _searcher.search(context)
+      
+      resource :indexes do
+        # curl  /v1/indexes?index_name=city:name&index_method=ngram&query=search%20string
+        get do
+          search
+        end
+        
+        post '/search' do
+          search
+        end
+        
+        post do
+          context = index_context
+          check_context!(:id, :dictionary_string, context)
+          _indexer(context[:index_method]).create_index(context)
+        end
+        
+        delete do
+          context = index_context
+          check_context!(:id, context)
+          _indexer(context[:index_method]).delete_index(context)
         end
       end
     end
