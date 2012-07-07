084e5b407e108b69b4927d03016d86c74d466649
undr <undr@yandex.ru>
2012-07-07 10:23:37 +0700

    Fixed 'ISO C90' problem

--- lib/fuzzzy/redis.rb
+++ lib/fuzzzy/redis.rb
@@ -12,24 +12,29 @@ _index_key(int argc, VALUE *argv, VALUE self)
 {
   VALUE type = rb_funcall(self, rb_intern("index_type"), 0);
   VALUE shared_key = rb_funcall(self, rb_intern("shared_key"), 0);
-  VALUE key, key2;
+  char sep[2] = ":";
+  VALUE key, key2, result;
+  char * buf;
+  unsigned long long length;
 
   if(rb_scan_args(argc, argv, "11", &key, &key2) == 2) {
-    char sep[2] = ":";
     key = rb_str_dup(key);
     rb_str_cat(key, sep, 1);
     rb_str_concat(key, rb_funcall(key2, rb_intern("to_s"), 0));
   }
 
-  int length = RSTRING_LEN(shared_key) + RSTRING_LEN(type) + RSTRING_LEN(key) + 4;
-  char buf[length];
-
+  length = RSTRING_LEN(shared_key) + RSTRING_LEN(type) + RSTRING_LEN(key) + 4;
+  buf = malloc(length);
   snprintf(buf, length, "%s:%s:%s", RSTRING_PTR(shared_key), RSTRING_PTR(type), RSTRING_PTR(key));
-  return rb_str_new2(buf);
+  result = rb_str_new2(buf);
+  free(buf);
+
+  return result;
 }
 EOC
     
     inline do |builder|
+      builder.add_compile_flags('-std=c99')
       builder.c_raw(INDEX_KEY, :method_name => 'index_key', :arity => -1)
     end
     
